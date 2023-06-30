3e65fb9036a6302ab1c610dfe6ab9fa5cbd95401
Praveen Kumar Chukkapalli <praveenkumarchukkapalli@Praveens-MacBook-Pro.local>
2023-06-30 11:26:51 -0500

    Fuel Quote Predictor Frontend Web page

--- fuel-app/src/pages/Home.js
+++ fuel-app/src/pages/Home.js
@@ -1,11 +1,66 @@
-import React from 'react';
- 
+import React, { useState } from 'react';
+import './Home.css';
+import { useNavigate } from "react-router-dom"
+import Header from './Header';
+import Footer from './Footer';
+
 const Home = () => {
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const navigate = useNavigate();
+    const [username, setUsername] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        navigate('/home')
+        // Here you can perform your login logic, such as sending the email and password to a server
+        console.log('Email:', username);
+    };
+
     return (
         <div>
-            <h1>This is home</h1>
+            <Header />
+            <br />
+            <center><h2>Fuel Quote Estimator</h2></center>
+            <div className="form-container">
+                <form onSubmit={handleSubmit}>
+                    <div>
+
+                        Delivery Address :
+                        <input type="text" value="8181 Fannin st" id="add-inp" disabled />
+
+                    </div>
+                    <br />
+                    <div id="ip-container">
+
+                        <div>
+                            Gallons Requested: &nbsp;
+                            <input type="number" step="any" id="gallons_inp_fld" value={username} onChange={handleUsernameChange} />
+                        </div>
+                        <div>
+                            Delivery Date: &nbsp;
+                        </div>
+                        <input type="date" />
+                    </div>
+                    <br />
+                    <div>
+                        Suggested Price:
+                        <input type="number" placeholder="$ 0.00" disabled />
+                    </div>
+                    <br />
+                    <div>
+                        Total Amount Due:
+                        <input type="number" placeholder="$ 0.00" disabled />
+                    </div>
+                    <br />
+                    <button type="submit">Get Quote</button>
+                </form>
+            </div>
+            <Footer />
         </div>
     );
 };
- 
+
 export default Home;
