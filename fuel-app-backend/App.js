ef3c5304269d88e3c15a94b9c48f8b2305dc4891
Praveen Kumar Chukkapalli <chukkapalli_praveen@srmap.edu.in>
2023-07-21 20:33:58 -0500

    UT update

--- fuel-app-backend/App.js
+++ fuel-app-backend/App.js
@@ -49,6 +49,71 @@ app.post('/clientProfile', (req, res) => {
         res.status(400).send({invalid_request: invalidMessage});
     }
     else {
+        pool.query('Select count(*) from verification_table where email=$1 and code=$2',[email, code], (error, results) => {
+            if(error) {
+                res.status(401).send(error);
+            }
+            else if(results?.rows[0]?.count === "0") {
+                res.status(409).send({"message":"Invalid code"});
+            }
+            else {
+                res.send({message:"code verified"});
+            }
+        })
+    }
+})
+
+app.post('/updatePassword', (req, res) => {
+    const {email, password} = req.body;
+    let invalidMessage = "";
+    if(!validateEmail(email)) {
+        invalidMessage += "Invalid Email address,"
+    }
+    if(!validate_feild(password, 7, 100)) {
+        invalidMessage += "The password length must be inbetween 7 and 100";
+    }
+    if(invalidMessage.length) {
+        res.status(400).send({invalid_request: invalidMessage});
+    }
+    else {
+        pool.query('Update register_table set password=$2 where email=$1', [email, password], (error, results) => {
+            if(error) {
+                res.status(401).send(error);
+            }
+            // console.log(results);
+            res.send({message:"success"});
+        })   
+    }
+})
+
+app.post('/clientProfile', (req, res) => {
+    const {email, firstname, lastname, address1, address2, city, state, zipcode} = req.body;
+    let invalidMessage = "";
+    if(!validateEmail(email)) {
+        invalidMessage += "Invalid Email address, "
+    }
+    if(!validate_feild(firstname, 1, 50)) {
+        invalidMessage += "firstname length must be inbetween 1 and 50, "
+    }
+    if(!validate_feild(lastname, 1, 50)) {
+        invalidMessage += "lastname length must be inbetween 1 and 50, "
+    }
+    if(!validate_feild(address1, 1, 100)) {
+        invalidMessage += "address1 length must be inbetween 1 and 100, "
+    }
+    if(!validate_feild(city, 1, 100)) {
+        invalidMessage += "city length must be inbetween 1 and 100, "
+    }
+    if(!validate_feild(state, 2, 2)) {
+        invalidMessage += "state length must be 2, "
+    }
+    if(!validate_feild(zipcode, 5, 9)) {
+        invalidMessage += "zipcode length must be inbetween 5 and 9"
+    }
+    if(invalidMessage.length) {
+        res.status(400).send({invalid_request: invalidMessage});
+    }
+    else {
         pool.query('Insert into user_table values($1,$2,$3,$4,$5,$6,$7,$8)', [firstname, lastname, address1, address2, city, state, zipcode, email], (error, results) => {
             if(error) {
                 res.status(401).send(error);
@@ -57,6 +122,70 @@ app.post('/clientProfile', (req, res) => {
             else {
                 res.send({message:"Client Profile Completed"});
     
+            }
+        })
+    }
+})
+
+app.get('/getUserDetails', (req, res) => {
+    const {email} = req.query;
+    let invalidMessage = "";
+    if(!validateEmail(email)) {
+        invalidMessage += "Invalid Email address,"
+    }
+    if(invalidMessage.length) {
+        res.status(400).send({invalid_request: invalidMessage});
+    }
+    else {
+        pool.query('Select address1, address2, state, city, zipcode from user_table where email=$1',[email], (error, results) => {
+            if(error) {
+                res.status(401).send(error);
+            }
+            else {
+                res.send(results?.rows[0]);
+            }
+        })
+    }
+})
+
+app.post('/submitQuote', (req, res) => {
+    const {email, gallons_requested, delivery_date, delivery_address, suggested_price_per_gallon, total_amount_due} = req.body;
+    let invalidMessage = "";
+    if(!validateEmail(email)) {
+        invalidMessage += "Invalid Email address,"
+    }
+    if(invalidMessage.length) {
+        res.status(400).send({invalid_request: invalidMessage});
+    }
+    else {
+        pool.query('Insert into fuel_quote_history values($1,$2,$3,$4,$5,$6)', [gallons_requested, delivery_address, delivery_date, suggested_price_per_gallon, total_amount_due, email], (error, results) => {
+            if(error) {
+                res.status(401).send(error);
+            }
+            // console.log(results);
+            else {
+                res.send({message:"Fuel Quote Submitted"});
+            }
+        })
+    }
+})
+
+app.get('/getFuelHistory', (req, res) => {
+    const {email} = req.query;
+    let invalidMessage = "";
+    if(!validateEmail(email)) {
+        invalidMessage += "Invalid Email address,"
+    }
+    if(invalidMessage.length) {
+        res.status(400).send({invalid_request: invalidMessage});
+    }
+    else {
+        pool.query('Select gallons_requested, delivery_date, delivery_address, suggested_price_per_gallon, total_amount_due from fuel_quote_history where email=$1',[email], (error, results) => {
+            if(error) {
+                res.status(401).send(error);
+            }
+            else {
+                res.send(results?.rows);
             }
         })
     }
--- fuel-app-backend/test.js
+++ fuel-app-backend/test.js
@@ -1,68 +1,321 @@
-// Test for /updatePassword endpoint
-  describe('POST /updatePassword', () => {
-    it('should return 400 and message "Invalid Email address," if email format is invalid', async () => {
+const request = require('supertest');
+const app = require('./App');
+const chaiAsPromised = require('chai-as-promised');
+const chai = require('chai');
+chai.use(chaiAsPromised);
+
+const { describe } = require('mocha');
+const expect = chai.expect;
+
+describe('API Endpoints', () => {
+
+  // Test for /register endpoint
+  describe('POST /register', () => {
+    it('should return 400 and message "Invalid Email address," if registration email format is invalid', async () => {
       const response = await request(app)
-        .post('/updatePassword')
+        .post('/register')
         .send({ email: 'yash', password: 'password' });
 
       expect(response.status).to.equal(400);
       expect(response.body).to.deep.equal({ invalid_request: 'Invalid Email address,' });
     });
 
-    it('should return 400 and message "The password length must be inbetween 7 and 100" if password length is insufficient', async () => {
+    it('should return 400 and message "The password length must be inbetween 7 and 100" if registration password length is insufficient', async () => {
       const response = await request(app)
-        .post('/updatePassword')
+        .post('/register')
         .send({ email: 'yashwanth0231@gmail.com', password: 'pass' });
 
       expect(response.status).to.equal(400);
       expect(response.body).to.deep.equal({ invalid_request: 'The password length must be inbetween 7 and 100' });
     });
 
-    it('should return 200 and message "success" if password is updated successfully', async () => {
+    it('should return 200 and message "success" if registration is successful', async () => {
       const response = await request(app)
-        .post('/updatePassword')
-        .send({ email: 'yashwanth0231@gmail.com', password: 'newpassword' });
+        .post('/register')
+        .send({ email: 'yashwanth0231@gmail.com', password: 'password' });
 
       expect(response.status).to.equal(200);
       expect(response.body).to.deep.equal({ message: 'success' });
     });
+
+    it('should return 409 with message "duplicate user" if user already exists', async () => {
+      const response = await request(app)
+        .post('/register')
+        .send({ email: 'yashwanth0231@gmail.com', password: 'password' });
+
+      expect(response.status).to.equal(409);
+      expect(response.body).to.deep.equal({ message: 'duplicate user' });
+    });
   });
 
-  // Test for /clientProfile endpoint
-  describe('POST /clientProfile', () => {
-    it('should return 400 and message "Invalid Email address, firstname length must be inbetween 1 and 50, lastname length must be inbetween 1 and 50, address1 length must be inbetween 1 and 100, city length must be inbetween 1 and 100, state length must be 2, zipcode length must be inbetween 5 and 9" if all client profile input validations fails', async () => {
+  describe('POST /login', () => {
+    it('should return 400 and message "Invalid Email address," if login email format is invalid', async () => {
       const response = await request(app)
-        .post('/clientProfile')
-        .send({
-          email: 'yashw',
-          firstname: '',
-          lastname: '',
-          address1: '',
-          address2: 'Apt 4B',
-          city: '',
-          state: 'NYWO',
-          zipcode: '345'
-        });
+        .post('/login')
+        .send({ email: 'yash', password: 'password' });
 
       expect(response.status).to.equal(400);
-      expect(response.body).to.deep.equal({ invalid_request: 'Invalid Email address, firstname length must be inbetween 1 and 50, lastname length must be inbetween 1 and 50, address1 length must be inbetween 1 and 100, city length must be inbetween 1 and 100, state length must be 2, zipcode length must be inbetween 5 and 9' });
+      expect(response.body).to.deep.equal({ invalid_request: 'Invalid Email address,' });
     });
 
-    it('should return 200 and message "Client Profile Completed" if client profile is inserted successfully', async () => {
+    it('should return 400 and message "The password length must be inbetween 7 and 100" if login password length is insufficient', async () => {
       const response = await request(app)
-        .post('/clientProfile')
+        .post('/login')
+        .send({ email: 'yashwanth0231@gmail.com', password: 'pass' });
+
+      expect(response.status).to.equal(400);
+      expect(response.body).to.deep.equal({ invalid_request: 'The password length must be inbetween 7 and 100' });
+    });
+
+    it('should return 200 and status "ok" with message "ClientProfilePending" if client profile is pending', async () => {
+      const response = await request(app)
+        .post('/login')
+        .send({ email: 'yashwanth0231@gmail.com', password: 'password' });
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal({ status: 'ok', message: 'ClientProfilePending' });
+    });
+
+    it('should return 401 with error message "Invalid Credentials" if invalid credentials are provided', async () => {
+      const response = await request(app)
+        .post('/login')
+        .send({ email: 'yashwanth0231@gmail.com', password: 'awfewel' });
+
+      expect(response.status).to.equal(401);
+      expect(response.body).to.deep.equal({ error: 'Invalid Credentials' });
+    });
+  });
+
+// Test for /forgot endpoint
+describe('POST /forgot', () => {
+  it('should return 400 and message "Invalid Email address," if email format is invalid', async () => {
+    const response = await request(app)
+      .post('/forgot')
+      .send({ email: 'yash' });
+
+    expect(response.status).to.equal(400);
+    expect(response.body).to.deep.equal({ invalid_request: 'Invalid Email address,' });
+  });
+
+  it('should return 200 and message "Email sent" if user is registered', async () => {
+    const response = await request(app)
+      .post('/forgot')
+      .send({ email: 'yashwanth0231@gmail.com' });
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal({ message: 'Email sent' });
+  });
+
+  it('should return 409 with message "user have not registered" if user is not registered', async () => {
+    const response = await request(app)
+      .post('/forgot')
+      .send({ email: 'unregistered@example.com' });
+
+    expect(response.status).to.equal(409);
+    expect(response.body).to.deep.equal({ message: 'user have not registered' });
+  });
+});
+
+// Test for /verify endpoint
+describe('POST /verify', () => {
+  it('should return 400 and message "Invalid Email address," if verification email format is invalid', async () => {
+    const response = await request(app)
+      .post('/verify')
+      .send({ email: 'yash', code: '11323' });
+
+    expect(response.status).to.equal(400);
+    expect(response.body).to.deep.equal({ invalid_request: 'Invalid Email address,' });
+  });
+
+  it('should return 400 and message "The verification code length should be 5" if verification code length is invalid', async () => {
+    const response = await request(app)
+      .post('/verify')
+      .send({ email: 'yashwanth0231@gmail.com', code: '113' });
+
+    expect(response.status).to.equal(400);
+    expect(response.body).to.deep.equal({ invalid_request: 'The verification code length should be 5' });
+  });
+
+  it('should return 200 and message "code verified" if valid email and code are provided', async () => {
+    const response = await request(app)
+      .post('/verify')
+      .send({ email: 'dummyemail4729529492@gmail.com', code: '12345' });
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal({ message: 'code verified' });
+  });
+
+  it('should return 409 with message "Invalid code" if invalid code is provided', async () => {
+    const response = await request(app)
+      .post('/verify')
+      .send({ email: 'yashwanth0231@gmail.com', code: '54321' });
+
+    expect(response.status).to.equal(409);
+    expect(response.body).to.deep.equal({ message: 'Invalid code' });
+  });
+});
+
+ // Test for /updatePassword endpoint
+ describe('POST /updatePassword', () => {
+  it('should return 400 and message "Invalid Email address," if email format is invalid', async () => {
+    const response = await request(app)
+      .post('/updatePassword')
+      .send({ email: 'yash', password: 'password' });
+
+    expect(response.status).to.equal(400);
+    expect(response.body).to.deep.equal({ invalid_request: 'Invalid Email address,' });
+  });
+
+  it('should return 400 and message "The password length must be inbetween 7 and 100" if password length is insufficient', async () => {
+    const response = await request(app)
+      .post('/updatePassword')
+      .send({ email: 'yashwanth0231@gmail.com', password: 'pass' });
+
+    expect(response.status).to.equal(400);
+    expect(response.body).to.deep.equal({ invalid_request: 'The password length must be inbetween 7 and 100' });
+  });
+
+  it('should return 200 and message "success" if password is updated successfully', async () => {
+    const response = await request(app)
+      .post('/updatePassword')
+      .send({ email: 'yashwanth0231@gmail.com', password: 'newpassword' });
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal({ message: 'success' });
+  });
+});
+
+// Test for /clientProfile endpoint
+describe('POST /clientProfile', () => {
+  it('should return 400 and message "Invalid Email address, firstname length must be inbetween 1 and 50, lastname length must be inbetween 1 and 50, address1 length must be inbetween 1 and 100, city length must be inbetween 1 and 100, state length must be 2, zipcode length must be inbetween 5 and 9" if all client profile input validations fails', async () => {
+    const response = await request(app)
+      .post('/clientProfile')
+      .send({
+        email: 'yashw',
+        firstname: '',
+        lastname: '',
+        address1: '',
+        address2: 'Apt 4B',
+        city: '',
+        state: 'NYWO',
+        zipcode: '345'
+      });
+
+    expect(response.status).to.equal(400);
+    expect(response.body).to.deep.equal({ invalid_request: 'Invalid Email address, firstname length must be inbetween 1 and 50, lastname length must be inbetween 1 and 50, address1 length must be inbetween 1 and 100, city length must be inbetween 1 and 100, state length must be 2, zipcode length must be inbetween 5 and 9' });
+  });
+
+  it('should return 200 and message "Client Profile Completed" if client profile is inserted successfully', async () => {
+    const response = await request(app)
+      .post('/clientProfile')
+      .send({
+        email: 'yashwanth0231@gmail.com',
+        firstname: 'John',
+        lastname: 'Doe',
+        address1: '123 Street',
+        address2: 'Apt 4B',
+        city: 'New York',
+        state: 'NY',
+        zipcode: '12345'
+      });
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal({ message: 'Client Profile Completed' });
+  });
+});
+
+  // Test for /getUserDetails endpoint
+  describe('GET /getUserDetails', () => {
+    it('should return 400 and message "Invalid Email address," if registration email format is invalid', async () => {
+      const response = await request(app)
+        .get('/getUserDetails')
+        .query({ email: 'yash' });
+
+      expect(response.status).to.equal(400);
+      expect(response.body).to.deep.equal({ invalid_request: 'Invalid Email address,' });
+    });
+
+    it('should return 200 and user details if email is valid', async () => {
+      const response = await request(app)
+        .get('/getUserDetails')
+        .query({ email: 'yashwanth0231@gmail.com' });
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal({
+        address1: '123 Street',
+        address2: 'Apt 4B',
+        state: 'NY',
+        city: 'New York',
+        zipcode: '12345'
+      });
+    });
+  });
+
+  // Test for /submitQuote endpoint
+  describe('POST /submitQuote', () => {
+    it('should return 400 and message "Invalid Email address," if email format is invalid', async () => {
+      const response = await request(app)
+        .post('/submitQuote')
+        .send({ email: 'yash' });
+
+      expect(response.status).to.equal(400);
+      expect(response.body).to.deep.equal({ invalid_request: 'Invalid Email address,' });
+    });
+
+    it('should return 200 and message "Fuel Quote Submitted" if quote is submitted successfully', async () => {
+      const response = await request(app)
+        .post('/submitQuote')
         .send({
           email: 'yashwanth0231@gmail.com',
-          firstname: 'John',
-          lastname: 'Doe',
-          address1: '123 Street',
-          address2: 'Apt 4B',
-          city: 'New York',
-          state: 'NY',
-          zipcode: '12345'
+          gallons_requested: 100,
+          delivery_date: '2023-07-15',
+          delivery_address: '123 Street',
+          suggested_price_per_gallon: 3.5,
+          total_amount_due: 350
         });
 
       expect(response.status).to.equal(200);
-      expect(response.body).to.deep.equal({ message: 'Client Profile Completed' });
+      expect(response.body).to.deep.equal({ message: 'Fuel Quote Submitted' });
     });
   });
+
+  describe('POST /login', () => {
+    it('should return 401 with error message "Invalid Credentials" if invalid credentials are provided', async () => {
+      const response = await request(app)
+        .post('/login')
+        .send({ email: 'yashwanth0231@gmail.com', password: 'awfewel' });
+
+      expect(response.status).to.equal(401);
+      expect(response.body).to.deep.equal({ error: 'Invalid Credentials' });
+    });
+  });
+
+  // Test for /getFuelHistory endpoint
+  describe('GET /getFuelHistory', () => {
+    it('should return 400 and message "Invalid Email address," if email format is invalid', async () => {
+      const response = await request(app)
+        .get('/getFuelHistory')
+        .query({ email: 'yash' });
+
+      expect(response.status).to.equal(400);
+      expect(response.body).to.deep.equal({ invalid_request: 'Invalid Email address,' });
+    });
+
+    it('should return 200 and fuel quote history for the user', async () => {
+      const response = await request(app)
+        .get('/getFuelHistory')
+        .query({ email: 'yashwanth0231@gmail.com' });
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal([
+        {
+          gallons_requested: 100,
+          delivery_date: '2023-07-15T05:00:00.000Z',
+          delivery_address: '123 Street',
+          suggested_price_per_gallon: 3.5,
+          total_amount_due: 350
+        }
+      ]);
+    });
+  });
+});
