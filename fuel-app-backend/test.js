9345633f9c115f348db917ab571278c56bd766a5
Praveen Kumar Chukkapalli <chukkapalli_praveen@srmap.edu.in>
2023-07-21 19:27:22 -0500

    UT update

--- fuel-app-backend/test.js
+++ fuel-app-backend/test.js
@@ -1,115 +1,68 @@
-const request = require('supertest');
-const app = require('./App');
-const chaiAsPromised = require('chai-as-promised');
-const chai = require('chai');
-chai.use(chaiAsPromised);
+// Test for /updatePassword endpoint
+describe('POST /updatePassword', () => {
+  it('should return 400 and message "Invalid Email address," if email format is invalid', async () => {
+    const response = await request(app)
+      .post('/updatePassword')
+      .send({ email: 'yash', password: 'password' });
 
-const { describe } = require('mocha');
-const expect = chai.expect;
-
-describe('API Endpoints', () => {
-
-  // Test for /register endpoint
-  describe('POST /register', () => {
-    it('should return 200 and message "success" if registration is successful', async () => {
-      const response = await request(app)
-        .post('/register')
-        .send({ email: 'newuser@example.com', password: 'password' });
-
-      expect(response.status).to.equal(200);
-      expect(response.body).to.deep.equal({ message: 'success' });
-    });
-
-    it('should return 409 with message "duplicate user" if user already exists', async () => {
-      const response = await request(app)
-        .post('/register')
-        .send({ email: 'newuser@example.com', password: 'password' });
-
-      expect(response.status).to.equal(409);
-      expect(response.body).to.deep.equal({ message: 'duplicate user' });
-    });
+    expect(response.status).to.equal(400);
+    expect(response.body).to.deep.equal({ invalid_request: 'Invalid Email address,' });
   });
 
-  describe('POST /login', () => {
-    it('should return 200 and status "ok" if valid credentials are provided', async () => {
-      const response = await request(app)
-        .post('/login')
-        .send({ email: 'newuser@example.com', password: 'password' });
+  it('should return 400 and message "The password length must be inbetween 7 and 100" if password length is insufficient', async () => {
+    const response = await request(app)
+      .post('/updatePassword')
+      .send({ email: 'yashwanth0231@gmail.com', password: 'pass' });
 
-      expect(response.status).to.equal(200);
-      expect(response.body).to.deep.equal({ status: 'ok' });
-    });
-
-    it('should return 200 and status "ok" with message "ClientProfilePending" if client profile is pending', async () => {
-      const response = await request(app)
-        .post('/login')
-        .send({ email: 'profilepending@example.com', password: 'password' });
-
-      expect(response.status).to.equal(200);
-      expect(response.body).to.deep.equal({ status: 'ok', message: 'ClientProfilePending' });
-    });
-
-    it('should return 401 with error message "Invalid Credentials" if invalid credentials are provided', async () => {
-      const response = await request(app)
-        .post('/login')
-        .send({ email: 'invalid@example.com', password: 'password' });
-
-      expect(response.status).to.equal(401);
-      expect(response.body).to.deep.equal({ error: 'Invalid Credentials' });
-    });
+    expect(response.status).to.equal(400);
+    expect(response.body).to.deep.equal({ invalid_request: 'The password length must be inbetween 7 and 100' });
   });
 
-  // Test for /forgot endpoint
-  describe('POST /forgot', () => {
-    it('should return 200 and message "Email sent" if user is registered', async () => {
-      const response = await request(app)
-        .post('/forgot')
-        .send({ email: 'registered@example.com' });
+  it('should return 200 and message "success" if password is updated successfully', async () => {
+    const response = await request(app)
+      .post('/updatePassword')
+      .send({ email: 'yashwanth0231@gmail.com', password: 'newpassword' });
 
-      expect(response.status).to.equal(200);
-      expect(response.body).to.deep.equal({ message: 'Email sent' });
-    });
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal({ message: 'success' });
+  });
+});
 
-    it('should return 409 with message "user have not registered" if user is not registered', async () => {
-      const response = await request(app)
-        .post('/forgot')
-        .send({ email: 'unregistered@example.com' });
+// Test for /clientProfile endpoint
+describe('POST /clientProfile', () => {
+  it('should return 400 and message "Invalid Email address, firstname length must be inbetween 1 and 50, lastname length must be inbetween 1 and 50, address1 length must be inbetween 1 and 100, city length must be inbetween 1 and 100, state length must be 2, zipcode length must be inbetween 5 and 9" if all client profile input validations fails', async () => {
+    const response = await request(app)
+      .post('/clientProfile')
+      .send({
+        email: 'yashw',
+        firstname: '',
+        lastname: '',
+        address1: '',
+        address2: 'Apt 4B',
+        city: '',
+        state: 'NYWO',
+        zipcode: '345'
+      });
 
-      expect(response.status).to.equal(409);
-      expect(response.body).to.deep.equal({ message: 'user have not registered' });
-    });
+    expect(response.status).to.equal(400);
+    expect(response.body).to.deep.equal({ invalid_request: 'Invalid Email address, firstname length must be inbetween 1 and 50, lastname length must be inbetween 1 and 50, address1 length must be inbetween 1 and 100, city length must be inbetween 1 and 100, state length must be 2, zipcode length must be inbetween 5 and 9' });
   });
 
-  // Test for /verify endpoint
-  describe('POST /verify', () => {
-    it('should return 200 and message "code verified" if valid email and code are provided', async () => {
-      const response = await request(app)
-        .post('/verify')
-        .send({ email: 'verified@example.com', code: '12345' });
+  it('should return 200 and message "Client Profile Completed" if client profile is inserted successfully', async () => {
+    const response = await request(app)
+      .post('/clientProfile')
+      .send({
+        email: 'yashwanth0231@gmail.com',
+        firstname: 'John',
+        lastname: 'Doe',
+        address1: '123 Street',
+        address2: 'Apt 4B',
+        city: 'New York',
+        state: 'NY',
+        zipcode: '12345'
+      });
 
-      expect(response.status).to.equal(200);
-      expect(response.body).to.deep.equal({ message: 'code verified' });
-    });
-
-    it('should return 409 with message "Invalid code" if invalid code is provided', async () => {
-      const response = await request(app)
-        .post('/verify')
-        .send({ email: 'verified@example.com', code: '54321' });
-
-      expect(response.status).to.equal(409);
-      expect(response.body).to.deep.equal({ message: 'Invalid code' });
-    });
-  });
-
-  // Test for /updatePassword endpoint
-  describe('POST /updatePassword', () => {
-    it('should return 200 and message "success" if password is updated successfully', async () => {
-      const response = await request(app)
-        .post('/updatePassword')
-        .send({ email: 'user@example.com', password: 'newpassword' });
-
-      expect(response.status).to.equal(200);
-      expect(response.body).to.deep.equal({ message: 'success' });
-    });
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal({ message: 'Client Profile Completed' });
   });
 });
